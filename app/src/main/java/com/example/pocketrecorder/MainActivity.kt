1319666cd23fa17a41e9f01f4ddf3aa874e0d741
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-03 00:04:11 +0600

    "InitialSetup"

--- app/src/main/java/com/example/pocketrecorder/MainActivity.kt
+++ app/src/main/java/com/example/pocketrecorder/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.example.pocketrecorder
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pocketrecorder.service.TapDetectionService
 import com.example.pocketrecorder.ui.theme.PocketRecorderTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,11 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PocketRecorderTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    Greeting("Android")
                 }
             }
         }
@@ -37,36 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
-    val context = LocalContext.current
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            Intent(context, TapDetectionService::class.java).also {
-                context.startForegroundService(it)
-            }
-        }) {
-            Text("Start Service")
-        }
-
-        Button(onClick = {
-            Intent(context, TapDetectionService::class.java).also {
-                context.stopService(it)
-            }
-        }) {
-            Text("Stop Service")
-        }
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreenPreview() {
+fun GreetingPreview() {
     PocketRecorderTheme {
-        MainScreen()
+        Greeting("Android")
     }
 }
