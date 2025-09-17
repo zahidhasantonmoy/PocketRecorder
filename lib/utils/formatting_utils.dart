517f88adb1e86c7a92a4071d594b8250854ad6ac
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-09-17 18:49:04 +0600

    Add formatting utilities for date and time

---
+++ lib/utils/formatting_utils.dart
@@ -0,0 +1,34 @@
+class FormattingUtils {
+  static String formatDuration(double seconds) {
+    final duration = Duration(milliseconds: (seconds * 1000).toInt());
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = duration.inHours;
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final secs = twoDigits(duration.inSeconds.remainder(60));
+
+    if (hours > 0) {
+      return '$hours:$minutes:$secs';
+    } else {
+      return '$minutes:$secs';
+    }
+  }
+
+  static String formatDate(DateTime date) {
+    final now = DateTime.now();
+    final difference = now.difference(date);
+
+    if (difference.inDays == 0) {
+      return 'Today';
+    } else if (difference.inDays == 1) {
+      return 'Yesterday';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays} days ago';
+    } else {
+      return '${date.day}/${date.month}/${date.year}';
+    }
+  }
+
+  static String formatDateTime(DateTime date) {
+    return '${date.day}/${date.month}/${date.year} at ${date.hour}:${date.minute.toString().padLeft(2, '0')}';
+  }
+}
