33917f47245b4edb3149938712cd2bad8dd18af1
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-09-17 14:45:51 +0600

    Update minSdkVersion to 24 for Android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.pocket_recorder"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 24
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
